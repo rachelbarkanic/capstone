a18ef95b5685c1ac32441f6564179d9cb48ded61
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-11-19 11:53:03 -0800

    gitignore

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const {SERVER_PORT} = process.env
 
 app.use(express.json())
 app.use(cors())
+
+const {seed} = require('./seed.js')
+
+app.post('/seed', seed)
+
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
