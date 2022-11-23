5d67be686b06343a30c0e9b4a0797e55fc3b6fd1
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-11-23 13:08:44 -0800

    random beer button done, tasting note drop down done

--- public/suggestion.js
+++ public/suggestion.js
@@ -1,4 +1,5 @@
 const noteSelect = document.querySelector('#note-select')
+const beerButton = document.getElementById("random-beer-button")
 
 const getNotes = () => {
     axios.get('http://localhost:4000/api/notes')
@@ -12,4 +13,15 @@ const getNotes = () => {
         })
 }
 
-    getNotes()
+const randomBeer = () => {
+    axios.get('http://localhost:4000/api/beers/random')
+    .then(res => {
+        const data = res.data.name;
+        alert("The style you should be drinking right now is: " + data.toUpperCase());
+    })
+}
+
+
+
+beerButton.addEventListener('click', randomBeer)
+getNotes()
